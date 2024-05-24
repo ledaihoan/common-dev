f7607dc12afb09ae58c59587a2c6695e382f0644
Hoan Le <ledaihoan@gmail.com>
2024-05-24 18:31:50 +0700

    feat(CORE-100): environment common development setup script

---
+++ 0-setup-base/install_env.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+./init_sudo.sh
+if [ -f /etc/os-release ]; then
+    # Get the ID field from /etc/os-release
+    OS=$(awk -F= '/^ID=/{print $2}' /etc/os-release | tr -d '"')
+else
+    echo "File /etc/os-release not found"
+    OS=unknown_os
+fi
+echo "Detected OS: $OS..."
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Check if the directory exists
+if [ -d "$CURRENT_DIR/$OS" ]; then
+    echo "Start installing $OS env..."
+else
+    echo "Currently, $OS is not officially supported yet...Exiting!"
+    exit 1
+fi
+"$CURRENT_DIR"/$OS/install_env.sh
